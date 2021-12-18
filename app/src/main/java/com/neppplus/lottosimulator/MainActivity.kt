d2f81fa4dec164b56f0952d10f022c9f4094bd9e
koolunkle <gugi329@daum.net>
2021-12-19 00:27:13 +0900

    임시(랜덤) 숫자 6개 생성  - 중복검사 수행 X

--- app/src/main/java/com/neppplus/lottosimulator/MainActivity.kt
+++ app/src/main/java/com/neppplus/lottosimulator/MainActivity.kt
@@ -1,12 +1,15 @@
 package com.neppplus.lottosimulator
 
 import android.os.Bundle
+import android.util.Log
 import androidx.databinding.DataBindingUtil
 import com.neppplus.lottosimulator.databinding.ActivityMainBinding
 
 class MainActivity : BaseActivity() {
 
     lateinit var binding: ActivityMainBinding
+
+    val mWinLottoNumArr = ArrayList<Int>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -16,6 +19,40 @@ class MainActivity : BaseActivity() {
     }
 
     override fun setupEvents() {
+
+        binding.btnBuyLotto.setOnClickListener {
+
+//            로또 번호 6개 생성
+            makeLottoNumbers()
+
+//            보너스번호 생성
+
+        }
+
+    }
+
+    fun makeLottoNumbers() {
+
+//        기존에 번호가 있다면 전부 삭제
+        mWinLottoNumArr.clear()
+
+//        6개의 당첨 번호 -> 반복 횟수 명확 => for
+        for (i in 0 until 6) {
+
+//            랜덤 숫자 추출 -> (제대로 된 숫자라면) 목록에 추가
+            while (true) {
+
+                val randomNum = (Math.random() * 45 + 1).toInt()
+
+                Log.d("랜덤", randomNum.toString())
+
+                if (true) {
+                    break
+                }
+
+            }
+
+        }
 
     }
 
